2816fa1d3db7666c6c8b468f2ecf6e94779dd5d7
Andy <andy@chiefmarley.com>
2012-01-05 19:51:09 -0600

    Added ability to run multiple times a day and only log once
    Since we were only running once a day, if that job failed, we would not record the usage.  This way we can run this script as many time a day as we want and we will only record the first time.

--- getYesterdaysUsage.py
+++ getYesterdaysUsage.py
@@ -4,17 +4,38 @@ from TStat import *
 import sqlite3
 from datetime import datetime, timedelta
 import time
+import os.path
 
-t = TStat('10.12.34.139')
-heatUsage = t.getHeatUsageYesterday()
-coolUsage = t.getCoolUsageYesterday()
+# get last succesful run time
+# we will touch this file if we succesfully run
+# if file has not yet been touched in the current day,
+# we will get the previous days usage
 
-heatMinutes = heatUsage['hour'] * 60 + heatUsage['minute']
-coolMinutes = coolUsage['hour'] * 60 + coolUsage['minute']
-currentTime = int(round(time.time()))
+FILE = 'getYesterdaysUsage'
+getUsage = False
+if not os.path.exists(FILE):
+    open(FILE,'a').close()
+    getUsage=True # getUsage if we have to create the file
 
-conn = sqlite3.connect('/home/andy/djangoProjects/leeHouseSite/sqlite/db.sql3')
-c = conn.cursor()
-c.execute("insert into restInterface_hvac_runtime values (NULL," + str(currentTime) + "," + str(heatMinutes) +"," + str(coolMinutes) + ")")
-conn.commit()
+# get last time FILE was modified
+lastSuccess =  time.ctime(os.path.getmtime(FILE))
+dateLastSuccess = datetime.strptime(lastSuccess,'%a %b %d %H:%M:%S %Y')
+
+#If file was modified on a day that is not today, update usage stats
+if dateLastSuccess.day != datetime.now().day:
+    getUsage = True
+
+if getUsage == True:
+    t = TStat('10.12.34.139')
+    heatUsage = t.getHeatUsageYesterday()
+    coolUsage = t.getCoolUsageYesterday()
+
+    heatMinutes = heatUsage['hour'] * 60 + heatUsage['minute']
+    coolMinutes = coolUsage['hour'] * 60 + coolUsage['minute']
+    currentTime = int(round(time.time()))
+
+    conn = sqlite3.connect('/home/andy/djangoProjects/leeHouseSite/sqlite/db.sql3')
+    c = conn.cursor()
+    c.execute("insert into restInterface_hvac_runtime values (NULL," + str(currentTime) + "," + str(heatMinutes) +"," + str(coolMinutes) + ")")
+    conn.commit()
 
