c66d70d331871bf1ba4461295143cecb039e7358
Andy <andy@chiefmarley.com>
2012-12-29 15:21:55 -0600

    Wrapped in try block
    Cron was spamming the mailbox when the connection failed,
    THis will fail quietly

--- getKitchenTemp.py
+++ getKitchenTemp.py
@@ -8,14 +8,16 @@ import sqlite3
 import time
 
 t = TStat('10.12.34.139')
-tempInFahr = t.getCurrentTemp()
-tempInKelv = ((tempInFahr - 32) *.555556) + 273.15
-tempInsertVal = int(round(tempInKelv*100))
+try:
+    tempInFahr = t.getCurrentTemp()
+    tempInKelv = ((tempInFahr - 32) *.555556) + 273.15
+    tempInsertVal = int(round(tempInKelv*100))
 
-currentTime = int(round(time.time()))
-conn = sqlite3.connect('/home/andy/djangoProjects/leeHouseSite/sqlite/db.sql3')
-c = conn.cursor()
-# 50 is sensor val for kitchen
-c.execute("insert into restInterface_temp_entry values (NULL," + str(currentTime) + ",50," + str(tempInsertVal) + ")")
-conn.commit()
-
+    currentTime = int(round(time.time()))
+    conn = sqlite3.connect('/home/andy/djangoProjects/leeHouseSite/sqlite/db.sql3')
+    c = conn.cursor()
+    # 50 is sensor val for kitchen
+    c.execute("insert into restInterface_temp_entry values (NULL," + str(currentTime) + ",50," + str(tempInsertVal) + ")")
+    conn.commit()
+except:
+    x = 1
